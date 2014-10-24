89d9a78c0db84c6fcb2de2777593c6354d101bac
Nino Siradze <ninome.siradze@gmail.com>
2014-10-24 12:49:18 +0400

    updated hw1

--- homework/submissions/nino_siradze/hw1/nino_siradze.rb
+++ homework/submissions/nino_siradze/hw1/nino_siradze.rb
@@ -1,14 +1,5 @@
 # Homework Assignment #1
 
-###
-
-# When you are done, add your file to your the git repository.
-# Then commit your changes with a relevant message.
-# Pull any new changes from the git repo first.
-# Then push your changes to the repo.
-###
-
-
 poem = "
   Jack and Jill went up the hill
   To fetch a pail of water.
