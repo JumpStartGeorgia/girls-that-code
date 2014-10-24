3dfffc8935f3ba1815d094be8bc51ceca6c128d0
Nino Siradze <ninome.siradze@gmail.com>
2014-10-24 12:48:21 +0400

    uploaded hw1

---
+++ homework/submissions/nino_siradze/hw1/nino_siradze.rb
@@ -0,0 +1,22 @@
+# Homework Assignment #1
+
+###
+
+# When you are done, add your file to your the git repository.
+# Then commit your changes with a relevant message.
+# Pull any new changes from the git repo first.
+# Then push your changes to the repo.
+###
+
+
+poem = "
+  Jack and Jill went up the hill
+  To fetch a pail of water.
+  Jack fell down and broke his crown,
+  And Jill came tumbling after.
+  
+  "
+
+new_poem = poem.lines.to_a.reverse.join
+
+puts new_poem;
