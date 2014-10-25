e9938db6eaa746ba6fcc04eb987017a424643c89
HannahCasey <h.borsody.casey@gmail.com>
2014-10-26 04:17:51 +1100

    homework1

---
+++ homework/submissions/hannah_casey/hannah_casey.rb
@@ -0,0 +1,11 @@
+poem = %q{
+  Jack and Jill went up the hill
+  To fetch a pail of water.
+  Jack fell down and broke his crown,
+  And Jill came tumbling after.
+  } 
+  
+new_poem = poem.lines.to_a.reverse.join
+
+puts new_poem
+  
