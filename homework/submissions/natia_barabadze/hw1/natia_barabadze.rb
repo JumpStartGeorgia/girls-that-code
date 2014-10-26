e645c21e207a111d4b6022246e412c408c1f4da9
nbarabadze <natia.barabadze@gmail.com>
2014-10-26 12:39:19 +0400

    homework1

---
+++ homework/submissions/natia_barabadze/hw1/natia_barabadze.rb
@@ -0,0 +1,15 @@
+#homework 1
+  
+poem="Jack and Jill went up the hill
+To fetch a pail of water.
+Jack fell down and broke his crown,
+And Jill came tumbling after."
+
+
+new_poem=poem.lines.to_a.reverse.join
+
+
+puts "new_poem"
+
+  
+
