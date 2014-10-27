539ddd6e06203f0faab993a542f565b1bb80b483
jason <jason.addie@gmail.com>
2014-10-27 13:10:43 +0400

    added feedback to hw1 submissions

--- homework/submissions/hannah_casey/hannah_casey.rb
+++ homework/submissions/hannah_casey/hannah_casey.rb
@@ -9,3 +9,9 @@ new_poem = poem.lines.to_a.reverse.join
 
 puts new_poem
   
+########################
+## Feedback from Jason
+## Good job!
+## A couple notes:
+## - Step 3 asked you to create a folder called hw1 and put this file inside of it
+## - you do not need .to_a - this was necessary in an older version of ruby but is not necessary after ruby 2.0
--- homework/submissions/lidia_momtselidze/hw1/lidia_momtselidze.rb
+++ homework/submissions/lidia_momtselidze/hw1/lidia_momtselidze.rb
@@ -10,3 +10,27 @@
   new_poem = poem.lines.to_a.reverse.join
 
   puts new_poem
+
+
+########################
+## Feedback from Jason
+## Good job!
+## A couple notes:
+## - you do not need .to_a - this was necessary in an older version of ruby but is not necessary after ruby 2.0
+## - you used %Q - this works but is not necessary
+##   - From class3 notes, you only need to use %Q if you are going 
+##     to have a variable inside your string like:
+# str8 = %Q{
+#   #{fname} #{lname} picked a peck of pickled peppers.
+#   A peck of pickled peppers #{fname} #{lname} picked.
+#   If #{fname} #{lname} picked a peck of pickled peppers,
+#   Where's the peck of pickled peppers that #{fname} #{lname} picked?
+# }
+##
+##   - Instead you could have used %q or just simply use "" like:
+#   poem = "
+#     Jack and Jill went up the hill
+#     To fetch a pail of water.
+#     Jack fell down and broke his crown,
+#     And Jill came tumbling after.
+#   "
