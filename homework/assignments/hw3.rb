e6be498ab59f165b824ad38223a5e23671a389b0
Eric N. Barrett <ericnbarrett@gmail.com>
2014-11-05 11:58:42 +0400

    added hw3.rb

---
+++ homework/assignments/hw3.rb
@@ -0,0 +1,69 @@
+# Homework Assignment #3
+# - Playing with arrays and building on what came before
+# - When in doubt, try: http://ruby-doc.org/core-2.1.3/Array.html
+
+##################
+#### IMPORTANT: after each step, use a puts statement to write out the variable(s) that you just created, like:
+#### puts "new_poem = #{new_poem}"
+#### 
+#### It will also be good to use a puts statement as a separator between each step, like:
+#### puts "------------------"
+#### puts "----- Question 3 -----"
+#### puts "------------------"
+####
+#### These are not required, but it does help make it easier to look at your output and know what your are looking at.
+##################
+
+###
+# 1. Please, make a string of as many Georgian names as possible.
+#    Ex. names_string = "Nino Mariam Jason Mariam Irakli Ia Anton Eric Elisabeth
+#                        Nathan Brian Tamo Kristine Giorgi"
+#        puts names_string    
+#
+#    Now, using what you have learned so far, turn that string into an array of
+#    names and assign it to the variable 'names'. Print that variable to the
+#    screen.
+#
+# 2. Please, repeat the first exercise, but create an array without creating a
+#    string first. There are several ways to create an array. Do it two
+#    different ways. Assign those arrays to the variables 'names1' and 'names2'.
+#    Print those variables to the screen.
+#
+# 3. Going back to the array 'names' from question 1, create a variable called
+#    'num_names' and insert it into a string that, in English, writes to the
+#    screen using a complete sentence the number of names you thought of. Use
+#    the 'uniq' method to ensure that you only have one of each name.
+#
+# 4. Now, go back to your names and create a nested array in which each name
+#    (they should all be unique at this point) also has a 'gender' attribute. 
+#    Assign this new array to the variable 'demographics'. Write to the screen
+#    the gender of the last person in your list using a complete sentence. 
+#
+# 5. Now, choose a name from your array 'names' and assign it to a variable  
+#    'random_name'. Now, using the 'names' array, find that name
+#    (remember question 4 from the previous homework assignment?), get its
+#    index number, and assign it to a variable called 'name_index'. Then, using
+#    your variables, write, using a complete English sentence, the name to the
+#    screen. It should read something like: "Mariam is my friend" 
+#
+# 6. Bonus Question!
+#
+#    Go back to question 5 and use the variable 'random_name' to delete that
+#    element in the array. This uses some methods from question 5 as well as a
+#    new method allowing you to use the index of an element in an array and
+#    delete that element. Use the Array API to find this method and use it.
+#    Write the new array to screen.    
+###
+
+### 
+# If you get stuck, don't worry. Try to work through it step-by-step. 
+# If you still can't figure out the solution, post a question in the 
+# girls-that-code repo issues:
+#
+# https://github.com/JumpStartGeorgia/girls-that-code/issues
+#
+# When you are done, add your file to your the git repository.
+# Then commit your changes with a relevant message.
+# Pull any new changes from the git repo first.
+# Then push your changes to the repo.
+###
