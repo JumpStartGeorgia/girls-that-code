da1dafe1a326dc105b5ce2244ba573937f02c62b
Eric N. Barrett <ericnbarrett@gmail.com>
2015-01-15 16:15:33 +0400

    Added homework #8.

---
+++ homework/assignments/hw8.rb
@@ -0,0 +1,40 @@
+# Homework Assignment #8
+# Using hashes and reviewing what came before
+# 
+# For this homework assignment we'll use the CSV file: makler_november.csv 
+# which lists details about property for sale and rent in november from Makler.
+# This file can be found in the directory datafiles in your repository
+
+# - if you need help with hashes, try here: http://www.ruby-doc.org/core-2.1.3/Hash.html
+# - if you need help with loading the csv, try here: http://ruby-doc.org/stdlib-2.1.3/libdoc/csv/rdoc/CSV.html
+
+##################
+#### IMPORTANT: after each step, use a puts statement to write out the variable(s) that you just created, like:
+#### puts "new_poem = #{new_poem}"
+#### 
+#### It will also be good to use a puts statement as a separator between each step, like:
+#### puts "------------------"
+#### puts "----- Step 3 -----"
+#### puts "------------------"
+####
+#### These are not required, but it does help make it easier to look at your output and know what your are looking at.
+##################
+
+
+###
+# 1. Load the Makler CSV file and assign it to the variable data. Be sure to indicate that your
+# data has a header when you load it.
+
+###
+# 2. Create a new variable called heating and assign to it all the posts that have central heating
+# and is an apartment.
+
+###
+# 3. If the posts in the heating variable has three or more rooms, assign the posts from question
+# to the variable big. If the posts has fewer than three rooms, assign them to the variable small.
+
+###
+# 4. For each variable big and small, what are the 5 most frequently used words used in the
+# column additional_info? Remember, you must clean the string first, then normalize it, before
+# you count. Refer to the notes from class if you can't remember how to do this. 
+
